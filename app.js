c8d35146023591d5bc877d920cbc1b26a89ef529
Giitbriel <115158978+Giitbriel@users.noreply.github.com>
2022-10-08 13:00:15 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const container = document.querySelector(".container");
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
 });
+
+  // To get the scroll position of current webpage
+  TopScroll = window.pageYOffset || document.documentElement.scrollTop;
+  LeftScroll = window.pageXOffset || document.documentElement.scrollLeft,
+  
+  // if scroll happens, set it to the previous value
+  window.onscroll = function() {
+  window.scrollTo(LeftScroll, TopScroll);
+          };
